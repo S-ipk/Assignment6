716c8014a7b5f1780e2e1158dae688ba664f0d56
S-ipk <63216041+S-ipk@users.noreply.github.com>
2020-10-09 16:42:19 +0100

    Safiye Ipek

---
+++ Question6.java
@@ -0,0 +1,29 @@
+package assignment6;
+
+public class Question6 {
+
+	public static void main(String[] args) {
+		
+		
+	
+		for (int i = 1; i <= 5; i++) {
+			
+			int num = i;
+			
+			for (int j = 1; j <= i ; j++) {
+				
+				System.out.print(num+ " ");
+				
+				num = num+5-j;
+			}
+			
+			System.out.println();
+		}
+		
+		
+
+		
+
+	}
+
+}
